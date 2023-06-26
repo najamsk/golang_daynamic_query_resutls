45c292af8aa9787b05603082f2eb4844a2336797
najam awan <najamsk@gmail.com>
2023-06-26 21:03:07 +0300

    more code

--- main.go
+++ main.go
@@ -16,8 +16,6 @@ import (
 //NOTE: https://forum.golangbridge.org/t/database-rows-scan-unknown-number-of-columns-json/7378/2
 //above is the reference for code sample.
 
-// TODO: should use gendric sql driver interface
-// and https://pkg.go.dev/github.com/lib/pq
 func main() {
 	urlExample := "postgres://user:password@localhost:5432/DelDB?sslmode=disable"
 	db, err := sql.Open("postgres", urlExample)
@@ -28,21 +26,74 @@ func main() {
 	defer db.Close()
 
 	//func conn
-	res, err := queryToJson(db, "select * from warriors")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "queryToJson failed: %v\n", err)
-		os.Exit(1)
-	}
-	log.Printf("res: %#v \n", string(res))
+
+	// Fiber setup and routes starting
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
+		res, err := queryToJson(db, "select * from warriors")
+		if err != nil {
+			log.Println("data error:", err)
+			return c.Status(500).JSON(&fiber.Map{
+				"success": false,
+				"error":   "something went wrong!",
+			})
+		}
+
+		log.Printf("res: %#v \n", string(res))
+		return c.SendString(string(res))
+	})
+
+	//simple endpoint that will scan values form db
+	app.Get("/simple", func(c *fiber.Ctx) error {
+		res, err := queryScanJson(db)
+		if err != nil {
+			log.Println("data error:", err)
+			return c.Status(500).JSON(&fiber.Map{
+				"success": false,
+				"error":   "something went wrong!",
+			})
+		}
+
 		return c.SendString(string(res))
 	})
 
 	log.Fatal(app.Listen(":3000"))
-
 }
+
+type Warrior struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Teacher   string `json:"teacher"`
+	IsActive  bool   `json:"isActive"`
+}
+
+func queryScanJson(db *sql.DB) ([]byte, error) {
+	res := []Warrior{}
+	q := "select first_name,last_name, teacher, is_active from warriors"
+
+	rows, err := db.Query(q)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		w := Warrior{}
+		var lName sql.NullString
+		// err := rows.Scan(&w.FirstName, &w.LastName, &w.Teacher, &w.IsActive)
+		err := rows.Scan(&w.FirstName, &lName, &w.Teacher, &w.IsActive)
+		if err != nil {
+			return nil, err
+		}
+		if lName.Valid {
+			w.LastName = lName.String
+		}
+		res = append(res, w)
+	}
+
+	return json.MarshalIndent(res, "", "\t")
+}
+
 func queryToJson(db *sql.DB, query string, args ...interface{}) ([]byte, error) {
 	// an array of JSON objects
 	// the map key is the field name
